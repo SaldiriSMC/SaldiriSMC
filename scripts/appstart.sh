c85873241b32832e4650b9cc9dd1d3a1af9e214c
Saldr <137400579+SaldiriSMC@users.noreply.github.com>
2023-12-21 12:46:12 +0500

    Update appstart.sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 export HOME=/home/ubuntu
-# Navigate to the project directory
 cd /home/ubuntu/saldirismc
-
-# Install dependencies
 sudo npm i
 sudo npm install bull
-# Display the list of PM2 processes
-pm2 list
-# Delete all PM2 processes
 pm2 delete all
-
-# Start the Node.js application with PM2
 pm2 start "npm run dev"
-
-# Display the list of PM2 processes
-pm2 list
-which pm2
-who | awk '$1 !~ /(:|pts|tty)/ {print $1}'
-
-# Sleep for 2 minutes
-sleep 2m
-
-# Display the list of PM2 processes again after 2 minutes
-pm2 list
